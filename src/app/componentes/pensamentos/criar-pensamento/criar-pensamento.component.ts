b8ff2123ae902b3bed26f03f13a08ebf224ca2f5
luizfiller <luiz.fsilva@sptech.school>
2023-03-06 11:38:13 -0300

    Update - criação de event binding para criar e cancelar nos botões

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -14,4 +14,12 @@ export class CriarPensamentoComponent {
     modelo: ''
   }
 
+  criarPensamento(){
+    alert(`Novo pensamento criado.`)
+  }
+
+  cancelarPensamento(){
+    alert(`Pensamento cancelado.`)
+  }
+
 }

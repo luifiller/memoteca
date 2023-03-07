296d5af84575becaa1e4c30c002b816e079120dd
luizfiller <luiz.fsilva@sptech.school>
2023-03-07 10:10:06 -0300

    Add - diretivas estruturais ngFor e ngIf para lista de pensamentos no mural

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -3,8 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-listar-pensamento',
   templateUrl: './listar-pensamento.component.html',
-  styleUrls: ['./listar-pensamento.component.css']
+  styleUrls: ['./listar-pensamento.component.css'],
 })
 export class ListarPensamentoComponent {
-
+  listaPensamentos = [
+    {
+      conteudo: 'Eu estou amando aprender Angular! :)',
+      autoria: 'Luiz Nison',
+      modelo: 'modelo3',
+    },
+    {
+      conteudo: 'Passo informações para o componente filho',
+      autoria: 'Componente pai',
+      modelo: 'modelo1',
+    },
+    {
+      conteudo: 'Minha propriedade é decorada com @Input()',
+      autoria: 'Componente filho',
+      modelo: 'modelo2  '
+    }
+  ];
 }

a1a06fbb53758fe0e47be3fcbc46c9659e61f0d7
luizfiller <luiz.fsilva@sptech.school>
2023-03-07 11:39:47 -0300

    Add - interface 'Pensamento', tipagem de objetos dos componentes com essa interface e import

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Pensamento } from '../pensamento/pensamento';
 
 @Component({
   selector: 'app-listar-pensamento',
@@ -6,26 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./listar-pensamento.component.css'],
 })
 export class ListarPensamentoComponent {
-  listaPensamentos = [
-    {
-      conteudo: 'Eu estou amando aprender Angular! :)',
-      autoria: 'Luiz Nison',
-      modelo: 'modelo3',
-    },
-    {
-      conteudo: 'Passo informações para o componente filho',
-      autoria: 'Componente pai',
-      modelo: 'modelo1',
-    },
-    {
-      conteudo: 'Minha propriedade é decorada com @Input()',
-      autoria: 'Componente filho',
-      modelo: 'modelo2'
-    },
-    {
-      conteudo: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident cupiditate molestiae distinctio sed quasi odit at nihil debitis ratione quas, vero iste atque facere tempora facilis quo eligendi voluptas expedita. Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque vitae, error iusto dolor numquam sunt rerum ad aliquid cupiditate nam corrupti atque aspernatur perferendis rem molestiae fugit repudiandae pariatur. Praesentium?',
-      autoria: 'Lorem Lorena',
-      modelo: 'modelo3'
-    }
-  ];
+  listaPensamentos: Pensamento[] = [];
 }
---
+++ src/app/componentes/pensamentos/pensamento/pensamento.ts
@@ -0,0 +1,7 @@
+// Utiliza-se "export" para que essa interface fique visível e possa ser utilizada por outras classes.
+export interface Pensamento {
+  id: number
+  conteudo: string
+  autoria: string
+  modelo: string
+}

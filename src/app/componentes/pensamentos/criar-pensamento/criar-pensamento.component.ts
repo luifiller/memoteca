eebd5d216f4e139a4929d7986629f8d0cc3a1ad8
luizfiller <luiz.fsilva@sptech.school>
2023-03-14 12:59:51 -0300

    Update - using FormGroup, FormBuilder to turn form into a Reactive Driven Form

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -2,6 +2,7 @@ import { PensamentoService } from './../pensamento.service';
 import { Pensamento } from './../pensamento/pensamento';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-criar-pensamento',
@@ -9,27 +10,30 @@ import { Router } from '@angular/router';
   styleUrls: ['./criar-pensamento.component.css'],
 })
 export class CriarPensamentoComponent {
-  constructor(private service: PensamentoService,
-    private router: Router
-    ) {}
+
+  constructor(
+    private service: PensamentoService,
+    private router: Router,
+    private formBuilder: FormBuilder
+  ) {}
+
+  formulario!: FormGroup;
 
   ngOnInit(): void {
-
+    this.formulario = this.formBuilder.group({
+      conteudo: ['Formulário Reativo'],
+      autoria: ['Angulaaar'],
+      modelo: ['modelo1'],
+    });
   }
 
-  pensamento: Pensamento = {
-    conteudo: '',
-    autoria: '',
-    modelo: 'modelo1',
-  };
-
   criarPensamento() {
-    this.service.criar(this.pensamento).subscribe(() => {
-      this.router.navigate(['/listarPensamento'])
+    this.service.criar(this.formulario.value).subscribe(() => {
+      this.router.navigate(['/listarPensamento']);
     });
   }
 
   cancelarPensamento() {
-    this.router.navigate(['/listarPensamento'])
+    this.router.navigate(['/listarPensamento']);
   }
 }
